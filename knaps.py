86a4962ee7f45ddd34d0d3258c7c4d289925958c
indyrajanuar <84507394+indyrajanuar@users.noreply.github.com>
2023-11-15 21:15:08 +0700

    knaps.py

--- knaps.py
+++ knaps.py
@@ -8,108 +8,13 @@ from sklearn.metrics import accuracy_score, classification_report
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 
-# Function to load and preprocess dataset
-def load_and_preprocess_data():
-    # Load dataset
-    url = "https://github.com/indyrajanuar/hipertensi/blob/main/datafix.csv"
-    df = pd.read_csv(url)
-
-    # Clean data (handle missing values, outliers, etc.)
-    # Assume the target variable is in the 'target' column
-    df_cleaned = df.dropna()  # Replace with your cleaning process
-
-    # Separate features and target
-    X = df_cleaned.drop(columns=['diagnosa'])
-    y = df_cleaned['diagnosa']
-
-    # One-hot encoding for categorical variables
-    categorical_features = ['categorical_column']  # Replace with your categorical column names
-    numeric_features = [col for col in X.columns if col not in categorical_features]
-
-    # Create transformers for preprocessing
-    numeric_transformer = Pipeline(steps=[
-        ('scaler', StandardScaler())
-    ])
-
-    categorical_transformer = Pipeline(steps=[
-        ('onehot', OneHotEncoder(handle_unknown='ignore'))
-    ])
-
-    # Combine transformers using ColumnTransformer
-    preprocessor = ColumnTransformer(
-        transformers=[
-            ('num', numeric_transformer, numeric_features),
-            ('cat', categorical_transformer, categorical_features)
-        ])
-
-    # Split the data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-    return X_train, X_test, y_train, y_test, preprocessor, df_cleaned
-
-# Function to train ERNN model
-def train_ernn(X_train, y_train):
-    # Create and train the ERNN model
-    mlp = MLPClassifier(
-        hidden_layer_sizes=(5,),
-        max_iter=1000,
-        learning_rate_init=0.1,
-        tol=0.0001,
-        random_state=42
-    )
-    
-    mlp.fit(X_train, y_train)
-    return mlp
-
-# Streamlit web app
-def main():
-    st.sidebar.title("Menu")
-    menu = st.sidebar.selectbox("Select Menu", ["Home", "Dataset", "Preprocessing", "Classification"])
-
-    if menu == "Home":
-        st.title("Home")
-        st.write("Deskripsi mengenai penyakit hipertensi.")
-
-    elif menu == "Dataset":
-        st.title("Dataset")
-        # Load and preprocess data
-        _, _, _, _, _, df_cleaned = load_and_preprocess_data()
-
-        # Display dataset
-        st.dataframe(df_cleaned)
-
-    elif menu == "Preprocessing":
-        st.title("Preprocessing")
-        # Load and preprocess data
-        X_train, _, _, _, _, _ = load_and_preprocess_data()
-
-        st.write("Dataset after Preprocessing:")
-        st.write("X_train shape:", X_train.shape)
-
-    elif menu == "Classification":
-        st.title("Classification")
-        # Load and preprocess data
-        X_train, X_test, y_train, y_test, preprocessor, _ = load_and_preprocess_data()
-
-        # Apply preprocessing to the training and testing sets
-        X_train_processed = preprocessor.fit_transform(X_train)
-        X_test_processed = preprocessor.transform(X_test)
-
-        # Train ERNN model
-        ernn_model = train_ernn(X_train_processed, y_train)
-
-        # Make predictions on the test set
-        y_pred = ernn_model.predict(X_test_processed)
-
-        # Evaluate the model
-        accuracy = accuracy_score(y_test, y_pred)
-        report = classification_report(y_test, y_pred)
-
-        # Display results
-        st.write("Model Evaluation:")
-        st.write("Accuracy:", accuracy)
-        st.write("Classification Report:")
-        st.text(report)
-
-if __name__ == "__main__":
-    main()
+from streamlit_option_menu import option_menu  #pustaka yang memberikan fungsi tambahan untuk membuat menu pilihan dengan Streamlit
+with st.sidebar: #Fungsi tersebut menghasilkan objek pilihan menu
+    choose = option_menu("Linear Regression (Polynomial)", ["Home", "Dataset", "Prepocessing", "Predict", "Help"],
+                             icons=['house', 'table', 'cloud-upload', 'boxes','check2-circle'],
+                             menu_icon="app-indicator", default_index=0,
+                             styles={
+            "container": {"padding":"5!important", "background-color": "10A19D"}, #Mengatur tampilan kontainer (wadah) dari menu pilihan
+            "icon": {"color": "blue", "font-size": "25px"},  #Mengatur tampilan ikon dalam menu pilihan. Properti "color" mengatur warna ikon. Properti "font-size" mengatur ukuran font ikon.
+            "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"}, #Mengatur tampilan tautan dalam menu pilihan
+            "nav-link-selected": {"background-color": "#00FFFF"}, #Mengatur tampilan tautan yang dipilih dalam menu pilihan

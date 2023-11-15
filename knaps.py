793540b7c0f7572d09b987acf618cfe548785084
indyrajanuar <84507394+indyrajanuar@users.noreply.github.com>
2023-11-15 21:34:13 +0700

    knaps.py

--- knaps.py
+++ knaps.py
@@ -1,11 +1,8 @@
 
 import streamlit as st
 import pandas as pd
-import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import accuracy_score, classification_report
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 
@@ -13,25 +10,30 @@ from sklearn.pipeline import Pipeline
 def load_and_preprocess_data():
     # Load dataset
     url = "https://raw.githubusercontent.com/indyrajanuar/hipertensi/main/datafix.csv"
-    df = pd.read_csv(url)
+    
+    try:
+        df = pd.read_csv(url)
+    except Exception as e:
+        st.error(f"Error reading dataset: {e}")
+        return None, None, None, None, None, None
 
-    # Clean data (handle missing values, outliers, etc.)
-    # Assume the target variable is in the 'target' column
-    df_cleaned = df.dropna()  # Replace with your cleaning process
+    # Clean data by removing categorical columns
+    # Assuming 'usia', 'sistole', 'diastole', 'nafas', 'detak_nadi' are non-categorical features
+    df_cleaned = df.drop(columns=['usia', 'sistole', 'diastole', 'nafas', 'detak_nadi'])
 
     # Separate features and target
-    X = df_cleaned.drop(columns=['diagnosa'])
+    X = df_cleaned.drop(columns=['jenis_kelamin', 'diagnosa'])
     y = df_cleaned['diagnosa']
 
-    # One-hot encoding for categorical variables
-    categorical_features = ['categorical_column']  # Replace with your categorical column names
-    numeric_features = [col for col in X.columns if col not in categorical_features]
+    # Numeric features after removing categorical columns
+    numeric_features = X.columns.tolist()
 
     # Create transformers for preprocessing
     numeric_transformer = Pipeline(steps=[
         ('scaler', StandardScaler())
     ])
 
+    categorical_features = ['jenis_kelamin']
     categorical_transformer = Pipeline(steps=[
         ('onehot', OneHotEncoder(handle_unknown='ignore'))
     ])
@@ -46,21 +48,16 @@ def load_and_preprocess_data():
     # Split the data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+    # Apply preprocessing to the training set
+    X_train_processed = preprocessor.fit_transform(X_train)
+
+    # Display the cleaned dataset after removing categorical columns
+    st.subheader("Cleaned Dataset (Categorical Columns Removed and One-Hot Encoded):")
+    st.dataframe(pd.DataFrame(X_train_processed, columns=numeric_features + preprocessor.transformers_[1][1]['onehot'].get_feature_names_out(categorical_features)))
+
     return X_train, X_test, y_train, y_test, preprocessor, df_cleaned
 
-# Function to train ERNN model
-def train_ernn(X_train, y_train):
-    # Create and train the ERNN model
-    mlp = MLPClassifier(
-        hidden_layer_sizes=(5,),
-        max_iter=1000,
-        learning_rate_init=0.1,
-        tol=0.0001,
-        random_state=42
-    )
-    
-    mlp.fit(X_train, y_train)
-    return mlp
+# ... (function for training and classification)
 
 # Streamlit web app
 def main():
@@ -76,41 +73,25 @@ def main():
         # Load and preprocess data
         _, _, _, _, _, df_cleaned = load_and_preprocess_data()
 
-        # Display dataset
+        # Display cleaned dataset after removing categorical columns
         st.dataframe(df_cleaned)
 
     elif menu == "Preprocessing":
         st.title("Preprocessing")
         # Load and preprocess data
-        X_train, _, _, _, _, _ = load_and_preprocess_data()
+        _, _, _, _, _, _ = load_and_preprocess_data()
 
-        st.write("Dataset after Preprocessing:")
-        st.write("X_train shape:", X_train.shape)
+        # The cleaned dataset after removing categorical columns is displayed within the load_and_preprocess_data function
 
     elif menu == "Classification":
         st.title("Classification")
         # Load and preprocess data
         X_train, X_test, y_train, y_test, preprocessor, _ = load_and_preprocess_data()
 
-        # Apply preprocessing to the training and testing sets
-        X_train_processed = preprocessor.fit_transform(X_train)
+        # Apply preprocessing to the testing set
         X_test_processed = preprocessor.transform(X_test)
 
-        # Train ERNN model
-        ernn_model = train_ernn(X_train_processed, y_train)
-
-        # Make predictions on the test set
-        y_pred = ernn_model.predict(X_test_processed)
-
-        # Evaluate the model
-        accuracy = accuracy_score(y_test, y_pred)
-        report = classification_report(y_test, y_pred)
-
-        # Display results
-        st.write("Model Evaluation:")
-        st.write("Accuracy:", accuracy)
-        st.write("Classification Report:")
-        st.text(report)
+        # ... (classification code)
 
 if __name__ == "__main__":
     main()

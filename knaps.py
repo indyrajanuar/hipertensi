c05d9ac57c2eb0148c3f339139558c8c7c1e25ee
indyrajanuar <84507394+indyrajanuar@users.noreply.github.com>
2023-11-15 20:20:34 +0700

    knaps.py

--- knaps.py
+++ knaps.py
@@ -8,7 +8,7 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score, classification_report
 
 # Load dataset
-url = "https://example.com/path/to/your/dataset.csv"
+url = "datafix.csv"
 df = pd.read_csv(url)
 
 # Data preprocessing

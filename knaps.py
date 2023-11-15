c32ecb173e508bc967bf16486d487161d83e99be
indyrajanuar <84507394+indyrajanuar@users.noreply.github.com>
2023-11-15 22:16:31 +0700

    knaps.py

--- knaps.py
+++ knaps.py
@@ -1,92 +1,47 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import accuracy_score, classification_report
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
 
-# Function to load and preprocess dataset
-def load_and_preprocess_data():
-    # Load dataset
-    url = "url = "https://raw.githubusercontent.com/indyrajanuar/hipertensi/main/datafix.csv""
-    df = pd.read_csv(url)
-
-    # Clean data (handle missing values, outliers, etc.)
-    # Assume the target variable is in the 'target' column
-    df_cleaned = df.dropna()  # Replace with your cleaning process
-
-    # Separate features and target
-    X = df_cleaned.drop(columns=['diagnosa'])
-    y = df_cleaned['diagnosa']
-
-    # One-hot encoding for categorical variables
-    categorical_features = ['categorical_column']  # Replace with your categorical column names
-    numeric_features = [col for col in X.columns if col not in categorical_features]
-
-    # Create transformers for preprocessing
-    numeric_transformer = Pipeline(steps=[
-        ('scaler', StandardScaler())
-    ])
-
-    categorical_transformer = Pipeline(steps=[
-        ('onehot', OneHotEncoder(handle_unknown='ignore'))
-    ])
-
-    # Combine transformers using ColumnTransformer
-    preprocessor = ColumnTransformer(
-        transformers=[
-            ('num', numeric_transformer, numeric_features),
-            ('cat', categorical_transformer, categorical_features)
-        ])
-
-    # Split the data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-    return X_train, X_test, y_train, y_test, preprocessor
-
-# Function to train ERNN model
-def train_ernn(X_train, y_train):
-    # Create and train the ERNN model
-    mlp = MLPClassifier(
-        hidden_layer_sizes=(5,),
-        max_iter=1000,
-        learning_rate_init=0.1,
-        tol=0.0001,
-        random_state=42
-    )
-    
-    mlp.fit(X_train, y_train)
-    return mlp
-
-# Streamlit web app
 def main():
-    st.title("Hipertensi Classification with ERNN")
+    st.title("Aplikasi Web Statis dengan Streamlit")
 
-    # Load and preprocess data
-    X_train, X_test, y_train, y_test, preprocessor = load_and_preprocess_data()
+    # Membuat objek pilihan menu menggunakan st.sidebar.selectbox
+    choose = st.sidebar("Choose", ["Home", "Dataset", "Preprocessing", "Evaluasi", "Klasifikasi", "Help"])
 
-    # Apply preprocessing to the training and testing sets
-    X_train_processed = preprocessor.fit_transform(X_train)
-    X_test_processed = preprocessor.transform(X_test)
+    if choose == "Home":
+        show_home()
+    elif choose == "Dataset":
+        show_dataset()
+    elif choose == "Preprocessing":
+        show_preprocessing()
+    elif choose == "Evaluasi":
+        show_evaluasi()
+    elif choose == "Klasifikasi":
+        show_klasifikasi()
+    elif choose == "Help":
+        show_help()
 
-    # Train ERNN model
-    ernn_model = train_ernn(X_train_processed, y_train)
+def show_home():
+    st.write("Selamat datang di halaman Home!")
+    # Tambahkan konten sesuai dengan halaman Home
 
-    # Make predictions on the test set
-    y_pred = ernn_model.predict(X_test_processed)
+def show_dataset():
+    st.write("Halaman Dataset")
+    # Tambahkan konten sesuai dengan halaman Dataset
 
-    # Evaluate the model
-    accuracy = accuracy_score(y_test, y_pred)
-    report = classification_report(y_test, y_pred)
+def show_preprocessing():
+    st.write("Halaman Preprocessing")
+    # Tambahkan konten sesuai dengan halaman Preprocessing
 
-    # Display results
-    st.write("Model Evaluation:")
-    st.write("Accuracy:", accuracy)
-    st.write("Classification Report:")
-    st.text(report)
+def show_evaluasi():
+    st.write("Halaman Evaluasi")
+    # Tambahkan konten sesuai dengan halaman Evaluasi
+
+def show_klasifikasi():
+    st.write("Halaman Klasifikasi")
+    # Tambahkan konten sesuai dengan halaman Klasifikasi
+
+def show_help():
+    st.write("Halaman Help")
+    # Tambahkan konten sesuai dengan halaman Help
 
 if __name__ == "__main__":
     main()

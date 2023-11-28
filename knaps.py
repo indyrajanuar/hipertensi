7889e28f48455ea908bec70e2028df1e2ad382bb
indyrajanuar <84507394+indyrajanuar@users.noreply.github.com>
2023-11-28 21:07:08 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -13,10 +13,13 @@ with st.sidebar:
     )
 
 with st.sidebar:
-    upload_file = st.sidebar.file_uploader("Masukkan file excel atau csv disini", key=1)
+    upload_files = st.sidebar.file_uploader("Masukkan file excel atau csv disini", key=1)
 
 if selected == 'Home':
     st.markdown('<h1 style="text-align: center;"> Website Klasifikasi Hipertensi </h1>', unsafe_allow_html=True)
+     for uploaded_file in uploaded_files:
+        df = pd.read_csv(uploaded_file)
+        st.dataframe(df)
 
 elif selected == 'PreProcessing Data':
     st.write("You are at PreProcessing Data")

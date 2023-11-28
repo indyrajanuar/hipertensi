6e67617f147526f5670d87313ce85911197b695f
indyrajanuar <84507394+indyrajanuar@users.noreply.github.com>
2023-11-28 21:02:30 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -12,20 +12,11 @@ with st.sidebar:
         orientation='vertical'
     )
 
+with st.sidebar:
+    upload_file = st.sidebar.file_uploader("Masukkan file excel atau csv disini", key=1)
+
 if selected == 'Home':
     st.markdown('<h1 style="text-align: center;"> Website Klasifikasi Hipertensi </h1>', unsafe_allow_html=True)
-    
-    # Display the uploaded file content
-    if 'upload_file' in st.session_state:
-        uploaded_file = st.session_state.upload_file
-        if uploaded_file is not None:
-            try:
-                # Try reading the CSV file with 'latin1' encoding
-                df = pd.read_csv(uploaded_file, encoding='latin1')
-                st.write("Uploaded File Content:")
-                st.write(df)
-            except UnicodeDecodeError:
-                st.error("Unable to decode the file. Please check the file encoding and try again.")
 
 elif selected == 'PreProcessing Data':
     st.write("You are at PreProcessing Data")
@@ -38,10 +29,3 @@ elif selected == 'Korelasi Data':
 
 elif selected == 'Uji Coba':
     st.write("You are at Uji Coba")
-
-with st.sidebar:
-    upload_file = st.sidebar.file_uploader("Masukkan file excel atau csv disini", key=1)
-
-    # Save the uploaded file to session state
-    if upload_file is not None:
-        st.session_state.upload_file = upload_file

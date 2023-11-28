a8b3a1f53d237377ca90d8585c5ea7a4b200233d
indyrajanuar <84507394+indyrajanuar@users.noreply.github.com>
2023-11-28 20:18:03 +0700

    knaps.py

--- knaps.py
+++ knaps.py
@@ -3,8 +3,9 @@ import pandas as pd
 import numpy as np 
 from streamlit_option_menu import option_menu
 
-selected = option_menu("Main Menu", ["Home", "PreProcessing Data", "Klasifikasi ERNN", "Korelasi Data", "Uji Coba"], icons=['house', 'table', 'boxes', 'boxes','check2-circle'], menu_icon="cast", default_index=1, orientation='vertical')
-selected
+with st.sidebar:
+    selected = option_menu("Main Menu", ["Home", "PreProcessing Data", "Klasifikasi ERNN", "Korelasi Data", "Uji Coba"], icons=['house', 'table', 'boxes', 'boxes','check2-circle'], menu_icon="cast", default_index=1, orientation='vertical')
+    selected
 
 if selected=='Home':
     st.write("You are at home")

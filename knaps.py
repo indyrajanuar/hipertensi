7dbcd416b31c031704ae09728818b89173b68f91
indyrajanuar <84507394+indyrajanuar@users.noreply.github.com>
2023-11-28 20:15:59 +0700

    knaps.py

--- knaps.py
+++ knaps.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np 
 from streamlit_option_menu import option_menu
 
-selected = om("Main Menu", ["Home", "PreProcessing Data", "Klasifikasi ERNN", "Korelasi Data", "Uji Coba"], icons=['house', 'table', 'boxes', 'boxes','check2-circle'], menu_icon="cast", default_index=1, orientation='vertical')
+selected = option_menu("Main Menu", ["Home", "PreProcessing Data", "Klasifikasi ERNN", "Korelasi Data", "Uji Coba"], icons=['house', 'table', 'boxes', 'boxes','check2-circle'], menu_icon="cast", default_index=1, orientation='vertical')
 selected
 
 if selected=='Home':

ecd9fa23710ddeb09679fecd702a03f5bf9ed69b
indyrajanuar <84507394+indyrajanuar@users.noreply.github.com>
2023-11-28 20:15:26 +0700

    knaps.py

--- knaps.py
+++ knaps.py
@@ -3,26 +3,20 @@ import pandas as pd
 import numpy as np 
 from streamlit_option_menu import option_menu
 
-with st.sidebar:
-    choose = option_menu("Main Menu", ["Home", "PreProcessing Data", "Klasifikasi ERNN", "Korelasi Data", "Uji Coba"],
-                             icons=['house', 'table', 'boxes', 'boxes','check2-circle'],
-                             menu_icon="app-indicator", default_index=0,
-                             styles={
-            "container": {"padding": "5!important", "background-color": "10A19D"},
-            "icon": {"color": "blue", "font-size": "25px"}, 
-            "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-            "nav-link-selected": {"background-color": "#00FFFF"},
-        }
-        )
-if choose=='Home':
+selected = om("Main Menu", ["Home", "PreProcessing Data", "Klasifikasi ERNN", "Korelasi Data", "Uji Coba"], icons=['house', 'table', 'boxes', 'boxes','check2-circle'], menu_icon="cast", default_index=1, orientation='vertical')
+selected
 
-elif choose=='PreProcessing Data':
+if selected=='Home':
+    st.write("You are at home")
+
+elif selected=='PreProcessing Data':
+    st.write("You are at home")
  
-elif choose=='Klasifikasi ERNN':
+elif selected=='Klasifikasi ERNN':
+    st.write("You are at home")
   
-elif choose=='Korelasi Data':
+elif selected=='Korelasi Data':
+    st.write("You are at home")
     
-elif choose == 'Uji Coba':
-
-if __name__ == "__main__":
-    main()
+elif selected=='Uji Coba':
+    st.write("You are at home")

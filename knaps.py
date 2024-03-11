285d1c66cba9f22645a0fe33971524d27c7deaf6
indyrajanuar <84507394+indyrajanuar@users.noreply.github.com>
2024-03-11 23:46:57 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -1,24 +1,25 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 import pandas as pd
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
 
 def preprocess_data(data): 
     # One-hot encoding for 'Jenis Kelamin'
     one_hot_encoder = OneHotEncoder()
     encoded_gender = one_hot_encoder.fit_transform(data[['Jenis Kelamin']].values.reshape(-1, 1))
-    encoded_gender = pd.DataFrame(encoded_gender.toarray(), columns=one_hot_encoder.get_feature_names_out(['Jenis Kelamin']))
-    
+    encoded_gender = pd.DataFrame(encoded_gender.toarray(), columns=one_hot_encoder.get_feature_names_out(['Jenis Kelamin']))    
     # Transform 'Diagnosa' feature to binary values
     data['Diagnosa'] = data['Diagnosa'].map({'YA': 1, 'TIDAK': 0})
-    
     # Drop the original 'Jenis Kelamin' feature
-    data = data.drop('Jenis Kelamin', axis=1)
-    
+    data = data.drop('Jenis Kelamin', axis=1)    
     # Concatenate encoded 'Jenis Kelamin' and transformed 'Diagnosa' with original data
     data = pd.concat([data, encoded_gender], axis=1)
-
     return data
+
+def normalize_data(data):
+    scaler = MinMaxScaler()
+    normalized_data = pd.DataFrame(scaler.fit_transform(data), columns=data.columns)
+    return normalized_data
     
 with st.sidebar:
     selected = option_menu(
@@ -52,7 +53,13 @@ elif selected == 'PreProcessing Data':
             preprocessed_data = preprocess_data(df)
             st.write("Transformation completed.")
             st.dataframe(preprocessed_data)
-            
+
+        st.markdown('<h3 style="text-align: left;"> Melakukan Normalisasi Data </h1>', unsafe_allow_html=True)
+        if st.button("Normalized Data"):
+            normalized_data = normalize_data(preprocessed_data)
+            st.write("Normalization completed.")
+            st.dataframe(normalized_data)
+
 elif selected == 'Klasifikasi ERNN':
     st.write("Berikut merupakan hasil klasifikasi yang di dapat dari pemodelan  Elman Recurrent Neural Network (ERNN)")
 

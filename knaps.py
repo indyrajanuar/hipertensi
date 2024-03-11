15db1c28d95cd4c6fe957c2c10c784cdb943ddad
indyrajanuar <84507394+indyrajanuar@users.noreply.github.com>
2024-03-11 14:28:19 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -1,15 +1,6 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 import pandas as pd
-import re
-from sklearn.preprocessing import LabelEncoder
-
-def label_encode_data(data, categorical_features):
-    label_encoder = LabelEncoder()
-    for feature in categorical_features:
-        if feature in data.columns:
-            data[feature] = label_encoder.fit_transform(data[feature])
-    return data
 
 with st.sidebar:
     selected = option_menu(
@@ -28,46 +19,13 @@ if selected == 'Home':
     st.markdown('<h3 style="text-align: left;"> Hipertensi </h1>', unsafe_allow_html=True)
     st.markdown('<h3 style="text-align: left;"> View Data </h1>', unsafe_allow_html=True)
     if upload_file is not None:
-        try:
-            df = pd.read_excel(upload_file, encoding='utf-8')
-        except UnicodeDecodeError:
-            try:
-                df = pd.read_excel(upload_file, encoding='latin1')
-            except UnicodeDecodeError:
-                df = pd.read_excel(upload_file, encoding='cp1252')
+        df = pd.read_excel(upload_file)
         st.write("Data yang digunakan yaitu data Penyakit Hipertensi dari UPT Puskesmas Modopuro Mojokerto.")
         st.dataframe(df)
 
 elif selected == 'PreProcessing Data':
     st.markdown('<h3 style="text-align: left;"> Data Asli </h1>', unsafe_allow_html=True)
     st.write("Berikut merupakan data asli yang didapat dari UPT Puskesmas Modopuro Mojokerto.")
-    
-    encoded_data = pd.DataFrame()  # Define encoded_data outside the 'One-Hot Encoding' block
-    
-    if upload_file is not None:
-        try:
-            df = pd.read_excel(upload_file, encoding='utf-8')
-        except UnicodeDecodeError:
-            try:
-                df = pd.read_excel(upload_file, encoding='latin1')
-            except UnicodeDecodeError:
-                df = pd.read_excel(upload_file, encoding='cp1252')
-        st.dataframe(df)
-
-        st.markdown('<h3 style="text-align: left;"> Melakukan Transformation Data </h1>', unsafe_allow_html=True)
-        # Specify the categorical features for label encoding
-        categorical_features = ['Jenis Kelamin', 'Diagnosa']
-        
-        if st.button("Label Encoding"):
-            encoded_data = label_encode_data(df, categorical_features)
-            st.write("Label encoding completed.")
-            st.dataframe(encoded_data)
-            st.write(encoded_data.shape)
-            st.write(encoded_data.dtypes)
-            st.write(encoded_data.isnull().sum())
-
-        st.markdown('<h3 style="text-align: left;"> Melakukan Normalisasi Data </h1>', unsafe_allow_html=True)
-        # Min-Max scaling for all features
                     
 elif selected == 'Klasifikasi ERNN':
     st.write("You are at Klasifikasi ERNN")

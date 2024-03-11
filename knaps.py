bd9c61c845bb43f53b45ba1891400b65f526e3ed
indyrajanuar <84507394+indyrajanuar@users.noreply.github.com>
2024-03-11 23:09:51 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -1,27 +1,6 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 import pandas as pd
-from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
-
-def preprocess_data(data):
-    # Replace commas with dots and convert numerical columns to floats
-    data['IMT'] = data['IMT'].replace({',': '.'}, regex=True).astype(float)
-    
-    # One-hot encoding for 'Jenis Kelamin'
-    one_hot_encoder = OneHotEncoder()
-    encoded_features = pd.DataFrame(one_hot_encoder.fit_transform(data[['Jenis Kelamin']]))
-    encoded_features.columns = one_hot_encoder.get_feature_names_out(['Jenis Kelamin'])
-    data = pd.concat([data.drop('Jenis Kelamin', axis=1), encoded_features], axis=1)
-    
-    # Transform 'Diagnosa' feature to '1' for 'YA' and '0' for 'TIDAK'
-    data['Diagnosa'] = data['Diagnosa'].map({'YA': 1, 'TIDAK': 0})
-    
-    return data
-
-def normalize_data(data):
-    scaler = MinMaxScaler()
-    data[data.columns] = scaler.fit_transform(data[data.columns])
-    return data
 
 with st.sidebar:
     selected = option_menu(
@@ -53,17 +32,6 @@ elif selected == 'PreProcessing Data':
         st.markdown('<h3 style="text-align: left;"> Melakukan Transformation Data </h1>', unsafe_allow_html=True)
 
         if st.button("Preprocess Data"):
-            preprocessed_data = preprocess_data(df)
-            st.write("Preprocessing completed.")
-            st.dataframe(preprocessed_data)
-            st.session_state.preprocessed_data = preprocessed_data  # Store preprocessed data in session state
-
-        st.markdown('<h3 style="text-align: left;"> Melakukan Normalisasi Data </h1>', unsafe_allow_html=True)
-        if 'preprocessed_data' in st.session_state:  # Check if preprocessed_data exists in session state
-            if st.button("Normalize Data"):
-                normalized_data = normalize_data(st.session_state.preprocessed_data.copy())
-                st.write("Normalization completed.")
-                st.dataframe(normalized_data)
 
 elif selected == 'Klasifikasi ERNN':
     st.write("Berikut merupakan hasil klasifikasi yang di dapat dari pemodelan  Elman Recurrent Neural Network (ERNN)")

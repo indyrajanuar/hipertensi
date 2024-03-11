d8aeb8eca41d530a9ff6f06821f50184a057c5b9
indyrajanuar <84507394+indyrajanuar@users.noreply.github.com>
2024-03-11 23:52:21 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -35,12 +35,15 @@ with st.sidebar:
 
     upload_file = st.sidebar.file_uploader("Masukkan file csv disini", key=1)
 
+# Load data if file uploaded
+if upload_file is not None:
+    df = pd.read_csv(upload_file)
+
 if selected == 'Home':
     st.markdown('<h1 style="text-align: center;"> Website Klasifikasi Hipertensi </h1>', unsafe_allow_html=True)
     st.markdown('<h3 style="text-align: left;"> Hipertensi </h1>', unsafe_allow_html=True)
     st.markdown('<h3 style="text-align: left;"> View Data </h1>', unsafe_allow_html=True)
     if upload_file is not None:
-        df = pd.read_csv(upload_file)
         st.write("Data yang digunakan yaitu data Penyakit Hipertensi dari UPT Puskesmas Modopuro Mojokerto.")
         st.dataframe(df)
 
@@ -49,7 +52,6 @@ elif selected == 'PreProcessing Data':
     st.write("Berikut merupakan data asli yang didapat dari UPT Puskesmas Modopuro Mojokerto.")
 
     if upload_file is not None:
-        df = pd.read_csv(upload_file)
         st.dataframe(df)
         st.markdown('<h3 style="text-align: left;"> Melakukan Transformation Data </h1>', unsafe_allow_html=True)
         if st.button("Transformation Data"):  # Check if button is clicked

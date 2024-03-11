f07e7a38cacb63cfc1f773d647bffa0e68f8acaa
indyrajanuar <84507394+indyrajanuar@users.noreply.github.com>
2024-03-11 23:18:08 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
 
 def preprocess_data(data): 
     # One-hot encoding for 'Jenis Kelamin'

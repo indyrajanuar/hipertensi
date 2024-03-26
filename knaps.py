e957141f8461b6fffb15af82bbecfb1d6696168b
indyrajanuar <84507394+indyrajanuar@users.noreply.github.com>
2024-03-26 20:07:21 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -157,7 +157,7 @@ elif selected == 'Klasifikasi ERNN':
 
         # Display the metrics
         html_header = '<h5 style="text-align: center; word-spacing: 5em"> Accuracy Precision Recall </h5>'
-        html_values = f'<div style="text-align: center; word-spacing: 8em;">{accuracy:.2f}% {precision:.2f}% {recall:.2f}%</div>'
+        html_values = f'<div style="text-align: center; word-spacing: 7.5em;">{accuracy:.2f}% {precision:.2f}% {recall:.2f}%</div>'
 
         st.markdown(html_header, unsafe_allow_html=True)
         st.markdown(html_values, unsafe_allow_html=True)
